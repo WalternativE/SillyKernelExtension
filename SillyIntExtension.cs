1fabf0f8d023d8e0a649632da87c551630fc9e96
WalternativE <gregor.beyerle@gmail.com>
2020-10-22 19:32:46 +0200

    Initial commit

---
+++ SillyIntExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Formatting;
+
+namespace SillyKernelExtension
+{
+    public class SillyIntExtension : IKernelExtension
+    {
+        public Task OnLoadAsync(Kernel rootKernel)
+        {
+            RegisterFormatters();
+
+            KernelInvocationContext.Current?.Display("Added a very silly Kernel.", "text/markdown");
+
+            return Task.CompletedTask;
+        }
+
+        private void RegisterFormatters()
+        {
+            Formatter.Register<System.Int32>((value, writer) =>
+            {
+                writer.Write($"Silly Number: {value}");
+            }, "text/plain");
+        }
+    }
+}
